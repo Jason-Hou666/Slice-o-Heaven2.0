5579e502b19d3cbf6c091a61104a3a26daeae329
Jason-Hou666 <781215041@qq.com>
2025-02-25 20:35:25 +0800

    Slice-o-Heaven2.0

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 public class App {
     public static void main (String[] args) {
-    AtmCard atm = new AtmCard();
-    AtmCard atm2 = new AtmCard("John Smith", 1234567890);
-    System.out.println("Account holder is:" + atm.getAccountHolder());
-    System.out.println("ATM Pin is:" + atm.getAtmPin());
-    System.out.println("Account holder is:" + atm2.getAccountHolder());
-    System.out.println("Account Pin is:" + atm2.getAtmPin());
-    System.out.println("Account Pin is:" + atm2.ATM_CHARGE);
+    SliceoHeavenOrder order = new SliceoHeavenOrder();
+    SliceoHeavenOrder order2= new SliceoHeavenOrder("DEF-SOH-099","Mozzarella Cheese",15.00);
+ 
+    System.out.println("OrderID is:" + order2.getOrderID());
+    System.out.println("PizzaIngredients is:" + order2.getPizzaIngredients());
+    System.out.println("OrderTotal is:" + String.format("%.2f",order.getOrderTotal()));
+ 
     
 }
     
